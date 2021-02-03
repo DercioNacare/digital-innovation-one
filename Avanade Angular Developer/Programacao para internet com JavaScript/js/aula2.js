a696e9eae3bc477388c1aa3832d6143b5fab67f8
Dercio Nacare <dpvnacare@gmail.com>
2021-02-03 10:15:42 +0200

    Array e dicionário

---
+++ Avanade Angular Developer/Programacao para internet com JavaScript/js/aula2.js
@@ -0,0 +1,11 @@
+var lista = ["Maçã", "Laranja", "Banana"];
+lista.push("Uva");
+lista.pop();
+console.log(lista);
+
+var fruta =  {nome: "Maçã", cor: "Verde"}
+console.log(fruta);
+
+var frutas = [{nome : "Maçã", cor: "Verde"}, {nome : "Banana", cor: "Verde"}]
+
+console.log(frutas)
