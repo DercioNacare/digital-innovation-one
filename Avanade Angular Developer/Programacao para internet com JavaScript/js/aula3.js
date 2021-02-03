71f267302aebd07fb11b6819c5b1f6819c9abd3b
Dercio Nacare <dpvnacare@gmail.com>
2021-02-03 10:31:19 +0200

    Condicionais, laços de repetição e Date

---
+++ Avanade Angular Developer/Programacao para internet com JavaScript/js/aula3.js
@@ -0,0 +1,25 @@
+var data = new Date();
+alert(data);
+
+
+for(var count = 0; count < 5; count ++)
+{
+    alert(count);
+}
+
+var count = 0;
+
+while(count < 5)
+{
+    console.log(count);
+    alert(count)
+    count ++;
+}
+var idade = prompt("Digite a sua idade: ");
+
+if(idade > 18)
+{
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
